41c9ad38df677ba158cf376d8e5f1d385724c982
2026m <2026m@Matt.unl.edu>
2025-08-27 09:28:56 -0500

    First commit

---
+++ CSCE155H/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Matthew Johnson
+ * 2025-08-25
+ * 
+ * Prints hello world to the standard output
+ */
+public class HelloWorld {
+	
+	public static void main(String[] Args) {
+		
+		System.out.printf("Hello World!\n");
+		
+	}
+}
